b17952f6b8fb364a75836788b1fe1fc82ab9a00d
julian-e-herrera <43296485+julian-e-herrera@users.noreply.github.com>
2020-11-06 14:13:50 -0300

    add login-popup error

--- client/js/login.js
+++ client/js/login.js
@@ -1,25 +1,34 @@
 async function validateLogin(e) {
+  let user = {
+    name: document.getElementById('username').value,
+    password: document.getElementById('password').value,
+  };
 
-    let user = {
-        "name": document.getElementById("username").value,
-        "password": document.getElementById("password").value
-    }
+  let respuesta = await fetch('/login/validate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  });
 
-    let respuesta = await fetch('/login/validate', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(user)
-    });
-
-    if (await respuesta.json()) {
-        window.sessionStorage.setItem("userLogged", true);
-        window.sessionStorage.setItem("user", user);
-        window.location.href = "http://localhost:3000";
-    }
-    //mostrar cartel de datos erroneos
+  if (await respuesta.json()) {
+    window.sessionStorage.setItem('userLogged', true);
+    window.sessionStorage.setItem('user', user);
+    window.location.href = 'http://localhost:3000';
+  }
+  //mostrar cartel de datos erroneos
+  else {
+    document.getElementById('username').focus();
+    document
+      .getElementById('login-button')
+      .setAttribute('data-toggle', 'modal');
+    document
+      .getElementById('login-button')
+      .setAttribute('data-target', '#myModal');
+  }
 }
 
-document.getElementById("login-button")
-    .addEventListener("click", validateLogin);
+document
+  .getElementById('login-button')
+  .addEventListener('click', validateLogin);
